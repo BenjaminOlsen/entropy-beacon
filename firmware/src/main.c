d5c0efb195838d84791874ac607250fbb396822c
Benjamin Olsen <thelarrys@gmail.com>
2026-03-08 08:39:38 +0100

    quality time range - one slider two knobs

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -125,7 +125,7 @@ int main(void)
                 lux_idx++;
             }
 
-            HAL_Delay(4);  
+            HAL_Delay(4);
         }
 
         /* Extract delta-sign bits from accel axes + lux */
